7c5fcbf69616c603e094c7d267978bf70b92fb39
dharmrajbhise <98268197+dharmrajbhise@users.noreply.github.com>
2023-07-19 18:56:11 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/CustomErrorController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+
+public class CustomErrorController implements ErrorController {
+
+//    @RequestMapping("/error")
+//    public String handleError() {
+//        // Custom error handling logic
+//        return "error"; // Replace with the name of your error page view template
+//    }
+//
+//
+//    public String getErrorPath() {
+//        return "/error";
+//    }
+}
